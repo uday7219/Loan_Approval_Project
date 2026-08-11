b896b074696478c67c33d26a074b50bc60db1ea9
Uday Sharma <ronak5243h@gmail.com>
2026-08-11 15:32:23 +0530

    update

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
-import os
 import pandas as pd
 from joblib import load
+from pathlib import Path
 
 # Load trained model
 #model = load("Model_Dir\\Loan_Model.joblib")
-from pathlib import Path
-import joblib
 
 BASE_DIR = Path(__file__).resolve().parent
 MODEL_PATH = BASE_DIR / "Model_Dir" / "Loan_Model.joblib"
